f41163993b88f498e89cbdf73e4d14010294bcf8
fatmaOsama <fatma.osama134@gmail.com>
2018-10-24 23:03:25 +0200

    Add Read File button and passing values to SimulationSystem

--- MultiQueueSimulation/MultiQueueSimulation/Form1.cs
+++ MultiQueueSimulation/MultiQueueSimulation/Form1.cs
@@ -14,11 +14,27 @@ namespace MultiQueueSimulation
 {
     public partial class Form1 : Form
     {
+        public SimulationSystem System;
+        public ReadFile Reader;
+
         public Form1()
         {
             InitializeComponent();
+            System = new SimulationSystem();
+            Reader = new ReadFile();
         }
 
-      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Reader.Read("E:\\FCIS 4th Year\\1st Sem\\Simulation\\Template\\MultiQueueSimulation\\MultiQueueSimulation\\TestCases\\TestCase1.txt");
+            System.NumberOfServers = Reader.Input.NumberOfServers;
+            System.StoppingNumber = Reader.Input.StoppingNumber;
+            System.StoppingCriteria =(MultiQueueModels.Enums.StoppingCriteria) Reader.Input.StoppingCriterea;
+            System.SelectionMethod =(MultiQueueModels.Enums.SelectionMethod)Reader.Input.SelectionMethod;
+            System.InterarrivalDistribution = Reader.InterarrivalDistribution;
+            //System.Servers = Reader.Input.service;
+            // Function that generates everything for all servers(System.Servers)
+        }
+     
     }
 }
