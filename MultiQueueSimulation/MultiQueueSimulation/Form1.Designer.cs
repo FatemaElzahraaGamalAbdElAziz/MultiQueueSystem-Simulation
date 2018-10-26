149129c0d78d07f150c4b07df8d1a624624b4cf5
fatmaOsama <fatma.osama134@gmail.com>
2018-10-26 10:52:55 +0200

    Fix Error

--- MultiQueueSimulation/MultiQueueSimulation/Form1.Designer.cs
+++ MultiQueueSimulation/MultiQueueSimulation/Form1.Designer.cs
@@ -37,14 +37,14 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(83, 32);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Read File";
+            this.button1.Text = "ReadFile";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+           // this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(212, 206);
             this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
